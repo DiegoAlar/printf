12f8961eed1645eb55ff73837c582d55fe36fa9f
Diego Alarcon <1153@holbertonschool.com>
2019-10-28 02:25:02 +0000

    adding file function_percentage_1 containing percentage format functions: s and c

--- function_percentage_1.c
+++ function_percentage_1.c
@@ -1,1 +1,53 @@
+#include "holberton.h"
 
+/**
+  * printChar - it prints a single char
+  *
+  *
+  *
+  */
+void printChar(va_list va)
+{
+	char c = (char) va_arg(va, int);
+	_putchar(c);
+}
+
+/**
+  * printString - function that prints a string
+  *
+  *
+  *
+  */
+void printString(va_list va)
+{
+	char *st = va_arg(va, char*);
+	int i = 0;
+
+	while (st[i])
+	{
+		_putchar(st[i]);
+		i++;
+	}
+}
+
+/**
+  * printInt - function that prints an integer
+  *
+  *
+  *
+  */
+void printInt(va_list va)
+{
+
+}
+
+/**
+  * printDec - function that prints a decimal number
+  *
+  *
+  *
+  */
+void printDec(va_list va)
+{
+
+}
