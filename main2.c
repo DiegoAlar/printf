aa21d745956c6312a4b246808bfd3694b5759def
Holberton <holberton@Holbertons-iMac-45.local>
2019-10-28 20:20:49 -0500

    modificate case %%%

---
+++ main2.c
@@ -0,0 +1,29 @@
+#include <limits.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+   * main - Entry point
+    *
+     * Return: Always 0
+      */
+int main(void)
+{
+	/*_printf("%s   \n");
+	_printf("%          s", "Holaaa!");
+	_printf("%"); */
+	_printf("%%%");
+	/*_printf(NULL);
+	*/
+	return (0);
+}
+
+
+
+
+
+
+
+
+
+
--- percenFunc.c
+++ percenFunc.c
@@ -11,13 +11,19 @@
 int funper(const char *format, int i, va_list toPrint)
 {
 	int c = 0;
+
 	tpPer relFormatFun[] = {
 		{'c', printChar		},
 		{'s', printString	},
 		{'i', printInt		},
 		{'d', printDec		},
 	};
-
+	
+	if (format[i + 1])
+	{
+		_putchar('%');
+		return (2);
+	}
 	while (c < 4)
 	{
 		if (relFormatFun[c].t == format[i + 1])
